7007a45820a6b8a10dfbaa113d6625dd7fcc6f49
jerem <jeremy.tellaa@tanker.io>
2019-07-25 10:50:18 +0200

    update to latest api

--- src/App.js
+++ src/App.js
@@ -1,63 +1,62 @@
 import React from 'react';
 
 import FileKit from '@tanker/filekit';
-import FakeAuth from '@tanker/fake-authentication';
+import FakeAuthentication from '@tanker/fake-authentication';
 
-import Upload from './Upload'
-import Download from './Download'
+import Upload from './Upload';
+import Download from './Download';
 
 
-const appId = 'qzORKAdxrYC/7mSeYTWsPMJkiyv1Vu61n5F/REvtUSk=';
-const url = 'https://staging-api.tanker.io';
+const appId = 'VoP7W4UypIz1/v9uouNYeWlcRizRPyqMkTnMtUs/dFw=';
 
 class App extends React.Component {
     constructor(props){
         super(props);
 
-        const fileKit = new FileKit({trustchainId: appId, url});
-        const fakeAuth = new FakeAuth(appId, 'https://staging-fakeauth.tanker.io');
+        const fileKit = new FileKit({ appId, url: 'https://dev-api.tanker.io' });
+        const fakeAuth = new FakeAuthentication({ appId, url: 'https://dev-fakeauth.tanker.io' });
 
         const urlParams = new URLSearchParams(window.location.search);
         const fileId = urlParams.get('fileId');
         const email = urlParams.get('email');
 
-        this.state = {fakeAuth, fileKit, fileId, email, ready: false};
-    }
-
-    async componentDidMount() {
-        if (this.state.email) {
-            const {privateIdentity, privateProvisionalIdentity} = await this.state.fakeAuth.getPrivateIdentity(this.state.email);
-            await this.state.fileKit.start(this.state.email, privateIdentity, privateProvisionalIdentity)
-        } else {
-          // Create a new identity with no email attached. This will be thrown away
-          const {privateIdentity} = await this.fakeAuth.getPrivateIdentity(this.fakeAuth.generateUserId());
-          console.log("start anonymous", privateIdentity);
-          await this.state.fileKit.startDisposableSession(privateIdentity);
-          console.log("status", this.fileKit.tanker.statusName);
-        }
-
-        this.setState({ready: true});
+        this.state = { fakeAuth, fileKit, fileId, email, ready: false };
     }
 
     downloadDone = () => {
-        this.setState({fileId: null});
+        this.setState({ fileId: null });
+    }
+
+    async componentDidMount() {
+        const { email } = this.state;
+        if (email) {
+          const privateIdentity = await this.state.fakeAuth.getPrivateIdentity(email);
+          await this.state.fileKit.start(email, privateIdentity);
+        } else {
+          // Create a new identity with no email attached. This will be thrown away
+          const privateIdentity = await this.state.fakeAuth.getPrivateIdentity();
+          console.log("start anonymous", privateIdentity);
+          await this.state.fileKit.startDisposableSession(privateIdentity);
+          console.log("status", this.state.fileKit.tanker.statusName);
+        }
+
+        this.setState({ ready: true });
     }
 
     render() {
-      if (!this.state.ready)
-        return (<center><p>Loading...</p></center>);
-      return (
-        <center>
-          {this.state.fileId ? (
+        if (!this.state.ready)
+            return <center><p>Loading...</p></center>;
+
+        return (
+          <center>
+            {this.state.fileId ? (
               <Download fileKit={this.state.fileKit} fileId={this.state.fileId} doneCb={this.downloadDone} />
             ) : (
-              <Upload fileKit={this.state.fileKit} fakeAuth={this.state.fakeAuth}/>
-          )}
-        </center>
-      );
+              <Upload fileKit={this.state.fileKit} fakeAuth={this.state.fakeAuth} />
+            )}
+          </center>
+        );
     }
-
-
 }
 
 export default App;
