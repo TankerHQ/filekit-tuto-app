018562986d39a21bcd2135c11787ccc12ffa1774
jerem <jeremy.tellaa@tanker.io>
2019-11-21 10:37:57 +0100

    fix(ci): use new Tanker app

--- src/config.dev.js
+++ src/config.dev.js
@@ -1,4 +1,4 @@
-export const appId = 'VoP7W4UypIz1/v9uouNYeWlcRizRPyqMkTnMtUs/dFw=';
+export const appId = 'TqegH/6vb8hC4g9p9pgMmjdpcKxx12Y+KrC7xX7PQ1A=';
 export const tankerApiUrl = 'https://dev-api.tanker.io';
 export const fakeAuthApiUrl = 'https://dev-fakeauth.tanker.io';
 export const appUrl = 'http://127.0.0.1:3000';
